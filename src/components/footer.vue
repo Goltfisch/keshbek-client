2c1f7820b9fcfa59770b8a2540b6009ce63d59b9
Daniel Wolf <daniel.wolf@8mylez.com>
2018-11-01 21:18:48 +0100

    Add components and base structure.

---
+++ src/components/footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="footer-container">
+        Footer
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {}
+        }
+    }
+</script>
+
+<style>
+    .footer-container {
+        height: 40px;
+        line-height: 40px;
+        position: fixed;
+        bottom: 0;
+        background: #00b2b2;
+        color: white;
+        width: 100%;
+    }
+</style>
--- src/layout/Layout.vue
+++ src/layout/Layout.vue
@@ -1,20 +1,57 @@
 <template>
     <div class="outer-wrapper">
         <div class="left">
-            <div class="sidebar"></div>
+            <div class="sidebar"> <component-sidebar></component-sidebar> </div>
         </div>
         <div class="right">
-            <div class="subnav"></div>
+            <div class="subnav"> <component-subnav></component-subnav> </div>
             <div class="content"> <router-view></router-view> </div>
-            <div class="footer"></div>
+            <div class="footer"> <component-footer></component-footer> </div>
         </div>
     </div>
 </template>
 
 <script>
+import ComponentSidebar from './../components/sidebar.vue';
+import ComponentSubnav from './../components/subnav.vue';
+import ComponentFooter from './../components/footer.vue';
+
 export default {
+    components: {
+        ComponentSidebar,
+        ComponentSubnav,
+        ComponentFooter
+    },
     data() {
         return {}
     }
 }
 </script>
+
+<style>
+    html {
+        height: 100%;
+    }
+
+    body {
+        margin: 0;
+        height: 100%;
+    }
+
+    .outer-wrapper {
+        display: table;
+        height: 100%;
+    }
+
+    .left {
+        background: #2a3b4d;
+        min-width: 200px;
+        display: table-cell;
+    }
+
+    .right {
+        background: #e9e9e9;
+        display: table-cell;
+        width: 100%;
+    }
+</style>

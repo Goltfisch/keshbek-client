f3b71b7bafd9a41288206c9e43a1bc2769e08b68
Daniel Wolf <daniel.wolf@8mylez.com>
2018-11-01 17:54:37 +0100

    Add router and three base files.

---
+++ src/layout/Layout.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="outer-wrapper">
+        <div class="left">
+            <div class="sidebar"></div>
+        </div>
+        <div class="right">
+            <div class="subnav"></div>
+            <div class="content"> <router-view></router-view> </div>
+            <div class="footer"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {}
+    }
+}
+</script>

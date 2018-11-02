a09ed39a0c5a2e0c0fc99a6aaea2550db8169713
Alexander Wolf <alexander@goltfisch.de>
2018-11-02 01:20:08 +0100

    Implement the client authentication

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import VueAuth from '@websanova/vue-auth';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 import Layout from './layout/Layout.vue';
 import Overview from './pages/Overview.vue';
@@ -14,14 +17,36 @@ const routes = [
     { path: '/', component: Overview },
     { path: '/login', component: Login },
     { path: '/profile', component: Profile }
-]
+];
 
 const router = new VueRouter({
     mode: 'history',
     routes
-})
+});
+
+Vue.router = router;
+
+Vue.use(VueAxios, axios);
+Vue.axios.defaults.baseURL = 'http://localhost:8000/api';
+
+Vue.use(VueAuth, {
+    //auth: require('@websanova/vue-auth/drivers/auth/bearer.js'),
+    auth: {
+        request: function (req, token) {
+            this.options.http._setHeaders.call(this, req, {Authorization: 'Bearer ' + localStorage.getItem('token')});
+        },
+        response: function (res) {
+            if (res.data.token) {
+                localStorage.setItem('token', res.data.token);
+                return res.data.token;
+            }
+        }
+    },
+    http: require('@websanova/vue-auth/drivers/http/axios.1.x.js'),
+    router: require('@websanova/vue-auth/drivers/router/vue-router.2.x.js'),
+});
 
 new Vue({
     router,
     render: h => h(Layout),
-}).$mount('#app')
+}).$mount('#app');
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,13 +1,43 @@
 <template>
     <div>
-        login
+        <form @submit="onLogin">
+            <label>Username</label></br>
+            <input type="text" name="_username" placeholder="Username" v-model="login._username" /></br></br>
+            <label>Password</label></br>
+            <input type="password" name="_password" placeholder="Password" v-model="login._password" /></br></br>
+            <button>Login</button>
+        </form>
     </div>
 </template>
 
 <script>
     export default {
         data() {
-            return {}
+            return {
+                login: {
+                    _username: '',
+                    _password: ''
+                },
+                authFaild: false
+            }
+        },
+        methods: {
+            onLogin: function(e) {
+                e.preventDefault();
+                
+                const data = {
+                    username: this.login._username, 
+                    password: this.login._password
+                };
+
+                this.$auth.login({
+                    data,
+                    error: (e) => {
+                        console.log(e);
+                        console.log('error');
+                    }
+                })
+            }
         }
     }
 </script>

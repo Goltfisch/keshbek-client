3eaae4883b5fc5216b7e49cc83f0de3f580d5d37
Daniel Wolf <daniel.wolf@8mylez.com>
2019-03-17 00:31:07 +0100

    Add basic form-component.

---
+++ src/components/Form.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="form-wrapper">
+        <form @submit.prevent="submitHandler()">
+            <table>
+                <tr class="input-group" v-for="input in inputs">
+                    <td><label :for="input.name">{{input.label}}</label></td>
+                    <td><input :type="input.type" :name="input.name" :value="input.value"></td>
+                </tr>
+            </table>
+            <button v-if="button">
+                {{button.text}}
+            </button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default{
+    data() {
+        return {}
+    },
+    props: {
+        inputs: {
+            type: Array,
+            required: true
+        },
+        button: {
+            type: Object,
+            required: false
+        },
+        submitHandler: {
+            type: Function,
+            required: false,
+            default: () => {}
+        }
+    }
+}
+</script>
+
+<style></style>
